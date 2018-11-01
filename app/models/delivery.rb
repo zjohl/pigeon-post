dfe06e90178be245b4a2df0629afdd45233dcf50
Michael Wong <emporophobe@gmail.com>
2018-11-01 15:27:39 -0700

    Updated delivery status enum

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -2,7 +2,7 @@ class Delivery < ApplicationRecord
   belongs_to :sender, :class_name => "User"
   belongs_to :receiver, :class_name => "User"
 
-  enum status: [:pending, :in_progress, :cancelled, :complete]
+  enum status: [:requested, :accepted, :confirmed, :in_progress, :cancelled, :completed]
 
   validates :drone_id, :status, :origin_latitude, :origin_longitude, :sender_id, :receiver_id, presence: true
 end
